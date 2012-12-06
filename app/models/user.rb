d0918784b25db973e9e3496fb27559902f455218
Greg Pallotta <gregpallotta@gmail.com>
2012-12-05 20:47:22 -0500

    add has_secure_password and validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,12 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
+
+  before_save { |user| user.email = email.downcase }
   
-  validates_presence_of :name, :email
+  validates_presence_of :name, :email, :password, :password_confirmation
   validates_uniqueness_of :email, :case_sensitive => false
+  validates_length_of :password, :minimum => 6
 end
