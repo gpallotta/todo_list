600ab9f971c1ab3e6a6dcab8edf4eb8b7e26678a
Greg Pallotta <gregpallotta@gmail.com>
2012-12-05 20:43:29 -0500

    add name and email validations to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  password_digest :string(255)
+#  remember_token  :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password_digest, :remember_token
+  attr_accessible :email, :name
+  
+  validates_presence_of :name, :email
+  validates_uniqueness_of :email, :case_sensitive => false
 end
