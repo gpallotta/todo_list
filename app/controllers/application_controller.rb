671a0bb54d49b8829008435d60510ba0ae6dd524
Greg Pallotta <greg@laptop.com>
2012-12-26 15:15:49 -0500

    add flash styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
-
-  def flash_to_headers
-    return unless request.xhr?
-    response.headers['X-Message'] = flash_message
-    response.headers['X-Message-Type'] = flash_type.to_s
-
-    flash.discard
-  end
-
-  private
-
-  def flash_message
-    [:error, :warning, :notice].each do |type|
-      return flash[type] unless flash[type].blank?
-    end
-  end
-
-  def flash_type
-    [:error, :warning, :notice].each do |type|
-      return type unless flash[type].blank?
-    end
-  end
 end
